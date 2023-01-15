fc7f992cda34627654e99316abea82673a38540f
Andrii Safonov <105196765+AndriySafonov@users.noreply.github.com>
2023-01-15 21:31:56 +0200

    Finished task-3

--- js/task-03.js
+++ js/task-03.js
@@ -1,19 +1,37 @@
 const images = [
   {
-    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-    alt: "White and Black Long Fur Cat",
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
   },
   {
-    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
   },
   {
-    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-    alt: "Group of Horses Running",
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
   },
 ];
 
-const galleryList = document.querySelector("ul.gallery")
-const listGallery = images.map((image) => `<li>image</li>`).join('')
+const galleryList = document.querySelector('.gallery');
+const listGallery = images
+  .map(
+    image =>
+      `<li class = 'list'><img class="imgCard" src=${image.url} width = '600' alt = ${image.alt}></img></li>`
+  )
+  .join('');
+console.log(galleryList);
 console.log(listGallery);
-galleryList.insertAdjacentHTML("beforeend", listGallery)
+galleryList.insertAdjacentHTML('beforeend', listGallery);
+const liEl = document.querySelector('.list');
+const imgEl = document.querySelector('.imgCard');
+console.log(liEl);
+console.log(imgEl);
+galleryList.style.display = 'flex';
+galleryList.style.gap = '10px';
+galleryList.style.listStyle = 'none';
+galleryList.style.margin = '0';
+galleryList.style.maxWidth = '100%';
+galleryList.style.flexDirection = 'column';
+
+
