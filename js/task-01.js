d508f4c99f9e9d451e085a3fdc7cec86596721f5
AndriySafonov <andreygold18@gmail.com>
2023-02-26 18:51:00 +0200

    fixed the first task

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,9 @@
 
-const NumberOfCategories = document.querySelectorAll(".item");
-console.log(item.value);
-NumberOfCategories.value.style.listStyle = none;
-console.log(`Number of categories:  ${NumberOfCategories.length}`);
+const numberOfCategories = document.querySelectorAll(".item");
 
-const categoryAndElements = NumberOfCategories.forEach((elem)=> {
-    console.log('Category: ', elem.firstElementChild.textContent);
-    console.log('Elements: ', elem.querySelectorAll('li').length);
-})
+console.log(`Number of categories: ${numberOfCategories.length}`);
+const category = numberOfCategories.forEach((el) => {
+  console.log(`Category: ${el.firstElementChild.textContent}`);
+  console.log(`Elements: ${el.lastElementChild.childElementCount}`);
+});
 
